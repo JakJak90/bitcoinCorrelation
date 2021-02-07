b851946308cd71102135aebeea1df28807dc23b8
JakJak90 <jason.kelderman@gmail.com>
2021-02-07 15:19:16 +0200

    Cleared up superfluous comments

--- bitcoin_correlation.py
+++ bitcoin_correlation.py
@@ -1,6 +1,8 @@
 # This project was done to gain a basic understanding of how Pandas works for data gathering
+# and analysis
+
 import pandas as pd # Data analysis package
 import json # Package required to use JSON data files
 from urllib.request import urlopen # Package enabling opening of websites and using APIs
-import yfinance as yf # Package grants access to Yahoo Finance data
-from datetime import datetime # Allows work with timestamps and other date formats
+import yfinance as yf # Yahoo Finance 
+from datetime import datetime 
