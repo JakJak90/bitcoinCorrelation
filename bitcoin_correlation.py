6f51237ae8be5c6890ff3b38ee194d553dfd7d76
JakJak90 <jason.kelderman@gmail.com>
2021-02-07 15:25:30 +0200

    Added code to retrieve Bitcoin data

--- bitcoin_correlation.py
+++ bitcoin_correlation.py
@@ -6,3 +6,9 @@ import json # Package required to use JSON data files
 from urllib.request import urlopen # Package enabling opening of websites and using APIs
 import yfinance as yf # Yahoo Finance 
 from datetime import datetime 
+
+# Pull bitcoin proce history
+btc_url = "https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=2000&api_key=d1875a3943f6f2ee83a90ac2e05d5fa018618e00724e9018f9bd08c0ac932cc6"
+btc_data = urlopen(btc_url).read() # Open the API contents 
+btc_json = json.loads(btc_data) # Output response in JSON format
+
